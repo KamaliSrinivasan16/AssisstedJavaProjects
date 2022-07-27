98effced15df697921afd08699e37d0f69c2c621
Kamali S <kamalisrinivasan504@gmail.com>
2022-07-27 11:53:08 +0530

    Add file via upload

--- src/programs/rangeQueries.java
+++ src/programs/rangeQueries.java
@@ -2,54 +2,28 @@ package programs;
 
 public class rangeQueries 
 {
-    static int k = 16;
-    static int N = 100000; 
-    static long table[][] = new long[N][k + 1]; 
-    
-    //to build a sparse table
-    static void buildSparseTable(int arr[], int n) 
-    { 
-        for (int i = 0; i < n; i++) 
-        {
-        	table[i][0] = arr[i];
-        }
-             
-        for (int j = 1; j <= k; j++)
-        {
-        	for (int i = 0; i <= n - (1 << j); i++)
-        	{
-        		table[i][j] = table[i][j - 1] + table[i + (1 << (j - 1))][j - 1];
-        	}
-        }
-         
-    } 
-    
-    //query calculation
-    static long query(int L, int R) 
-    {
-        long answer = 0; 
-        for (int j = k; j >= 0; j--)  
-        { 
-            if (L + (1 << j) - 1 <= R)  
-            { 
-                answer = answer + table[L][j];
-                L += 1 << j; 
-            } 
-        } 
-        return answer; 
-    }
-
-    //main function
+    //to calculate sum of the given range
+	public static int sum(int[] arr,int n, int start, int end)
+	{
+		int sum =0;
+		for(int i=start;i<=end;i++)
+		{
+			sum +=arr[i];
+		}
+		return sum;
+	}
+	
+	//main function
 	public static void main(String[] args) 
 	{
-        int arr[] = { 3, 7, 2, 5, 8, 9 }; 
-        int n = arr.length; 
-        buildSparseTable(arr, n); 
-        System.out.println(query(0, 5)); 
-        System.out.println(query(3, 5)); 
-        System.out.println(query(2, 4)); 
+	     int arr[] = { 3, 7, 2, 5, 8, 9 }; 
+	     int n = arr.length; 
+	       
+	     System.out.println(sum(arr,n,0, 5)); 
+	     System.out.println(sum(arr,n,3, 5)); 
+	     System.out.println(sum(arr,n,2, 4)); 
 
-		
+			
 	}
 	
 }
